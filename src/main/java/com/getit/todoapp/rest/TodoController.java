c81f68f038358c5032af47045aea0402fa608529
kiranreddykasa <kiranreddy2004@gmail.com>
2013-09-27 16:28:57 +0530

    added logout button

--- src/main/java/com/getit/todoapp/rest/TodoController.java
+++ src/main/java/com/getit/todoapp/rest/TodoController.java
@@ -2,8 +2,6 @@ package com.getit.todoapp.rest;
 
 import java.util.List;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -37,25 +35,23 @@ public class TodoController {
 
 
 	@RequestMapping(method = RequestMethod.DELETE, headers = "Accept=application/json")
-	public ResponseEntity<String> deleteAllTodos() {
-		List<Todo> todos = todoService.findAllTodoes();
+	public ResponseEntity<String> deleteAllTodos(Authentication authentication) {
+		 User user=(User) authentication.getPrincipal(); 
+		 List<Todo> todos = todoService.findTodosByUserName(user.getUsername());
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-Type", "application/json");
 		for (Todo todo : todos) {
+			todo.setUserName(null);
 			todoService.deleteTodo(todo);
 		}
 		return new ResponseEntity<String>(headers, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, headers = "Accept=application/json")
-    public ResponseEntity<String> updateFromJson(@RequestBody String json, @PathVariable("id") Long id) throws JSONException {
+    public ResponseEntity<String> updateFromJson(@RequestBody String json, @PathVariable("id") Long id) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
-        JSONObject request=new JSONObject(json);
-        Userinfo userinfo=userService.findUserinfo(Long.parseLong(request.get("userName").toString()));
-        JSONObject userJsonObject=new JSONObject(userinfo.toJson());
-        request.put("userName", userJsonObject);
-        Todo todo = Todo.fromJsonToTodo(request.toString());
+        Todo todo = Todo.fromJsonToTodo(json.toString());
         if (todoService.updateTodo(todo) == null) {
             return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
         }
@@ -114,6 +110,7 @@ public class TodoController {
         if (todo == null) {
             return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
         }
+        todo.setUserName(null);
         todoService.deleteTodo(todo);
         return new ResponseEntity<String>(headers, HttpStatus.OK);
     }
